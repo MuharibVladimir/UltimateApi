4f7f0b3886ad6eef3edb0fda3fc3a5a7b98423fe
Vladimir Turomsha <zver010810@gmail.com>
2023-01-03 22:22:28 +0300

    add service layer

---
+++ Services/EmployeeService.cs
@@ -0,0 +1,25 @@
+﻿using Contracts;
+using LoggerService;
+using Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    internal sealed class EmployeeService: IEmployeeService
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly ILoggerManager _logger;
+        public EmployeeService(IRepositoryManager repository, ILoggerManager
+        logger)
+        {
+            _repository = repository;
+            _logger = logger;
+        }
+
+    }
+}
+    
--- UltimateApi/Extensions/ServiceExtensions.cs
+++ UltimateApi/Extensions/ServiceExtensions.cs
@@ -1,6 +1,9 @@
 ﻿using Contracts;
 using LoggerService;
 using Repositories;
+using Services.Contracts;
+using Services;
+using Microsoft.EntityFrameworkCore;
 
 namespace UltimateApi.Extensions
 {
@@ -25,5 +28,13 @@ namespace UltimateApi.Extensions
 
         public static void ConfigureRepositoryManager(this IServiceCollection services) =>
             services.AddScoped<IRepositoryManager, RepositoryManager>();
+
+        public static void ConfigureServiceManager(this IServiceCollection services) =>
+            services.AddScoped<IServiceManager, ServiceManager>();
+
+        public static void ConfigureSqlContext(this IServiceCollection services, IConfiguration configuration) =>
+            services.AddDbContext<RepositoryContext>(opts =>
+                opts.UseSqlServer(configuration.GetConnectionString("sqlConnection")));
+
     }
 }
